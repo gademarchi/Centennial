94cfc88cec7cfb3f6c95b3d9d2264e6b05e51dd5
Gabriel <Gabriel@GABRIEL-PC>
2017-06-29 11:46:19 -0400

    start

---
+++ Week9/Person/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    class Person
+    {
+
+        //Private instance variable(fields)
+        private string _name;
+        private int _age;
+
+        //public properties----------------
+        public string Name{
+            get
+            { 
+
+       
+           
+        {
+
+        }
+
+    }
+}
