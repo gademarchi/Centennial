eb162cf25f09dd242e55ab603688ea84726ef8a4
Gabriel <Gabriel@GABRIEL-PC>
2017-06-29 12:20:31 -0400

    DomesticStudents

---
+++ Week9/Person/DomesticStudent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Person
+{
+    public class DomesticStudent : Student
+    {
+        // PUBLIC PROPERTIES
+
+        public string DomesticAddress { get; set; }
+
+        // CONSTRUCTORS ---------------------------
+
+        public DomesticStudent(string name, int age, string studentID)
+            : base(name, age, studentID)
+        {
+            this.DomesticAddress = "Unknown";
+        }
+
+    }
+}
