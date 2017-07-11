c70ecaeb873fdd34b5be6b13bcc455c7e36e483e
Gabriel <Gabriel@GABRIEL-PC>
2017-07-11 15:27:16 -0400

    create class human

---
+++ Week9/COMP123_week10/Human.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COMP123_week10
+{
+    /// <summary>
+    /// This is the human abstract class
+    /// </summary>
+    public abstract class Human
+    {
+        //public fields
+        private string _name;
+
+
+        //public properities
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+
+        }
+        
+
+       //constructors
+       /// <summary>
+       /// This is the constructor
+       /// </summary>
+
+        public Human()
+        {
+            this.Name = name;
+
+        }
+
+
+
+    }
+}
