b52c2a728fcffd320b1de0723e67b1e9199eeb97
Gabriel <Gabriel@GABRIEL-PC>
2017-07-11 15:41:54 -0400

    create the SuperHuman sub class

---
+++ Week9/COMP123_week10/SuperHUman.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Gabriel De Marchi
+ * Date: July 11,2017
+ * Description: This is the SuperHUman sub class
+ *
+ */
+
+
+namespace COMP123_week10
+{
+    /// <summary>
+    /// This is the superHuman sub class.
+    /// Its a inherits from the human superclass
+    /// </summary>
+    public class SuperHUman :Human
+    {
+        //private fields
+
+        //private properties
+
+        //constructors
+        public SuperHUman(string name)
+            :base(name)
+        {
+
+        }
+
+        //public methids
+
+
+    }
+
+}
