c4870ef96adfe7867c68269fa6ccacd7d2888860
Gabriel <Gabriel@GABRIEL-PC>
2017-07-11 15:46:52 -0400

    create power class

---
+++ Week9/COMP123_week10/Power.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Gabriel De mArchi
+ * Date: july 11,2017
+ */
+
+
+namespace COMP123_week10
+{
+    /// <summary>
+    /// The power class containts the data and structure
+    /// </summary>
+
+    public class Power
+    {
+        public string Name { get; set; }
+        public int Rank { get; set; }
+
+    }
+}
