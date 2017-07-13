f826093fc348cbb1c0feb3e1be4709af3b38257e
Gabriel <Gabriel@GABRIEL-PC>
2017-07-13 12:02:13 -0400

    create IHasDarma and SupervVillain Classes

--- Week9/COMP123_week10/IHasDarma.cs
+++ Week9/COMP123_week10/IHasDarma.cs
@@ -4,18 +4,18 @@ using System.Linq;
 using System.Text;
 
 /*
- * Name: Gabriel
+ * Name: Gabriel De Marchi
  * Date: July 13, 2017
  * Description: This Interface lists the signature for any Super Hero Subclass
- * Version: 0.1 - declared the Karma property
+ * Version: 0.1 - declared the Darma property
  */
 
 namespace COMP123_week10
 {
 
-    public interface IHasKarma
+    public interface IHasDarma
     {
         // PUBLIC PROPERTIES
-        int Karma { get; set; }
+        int Darma { get; set; }
     }
 }
---
+++ Week9/COMP123_week10/SuperVillain.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Tom Tsiliopoulos
+ * Date: July 13, 2017
+ * Description: This is the SuperHero class which inherits from the SuperHuman class
+ * Version: 0.1 - implemented the ToString method of the superhuman class
+ */
+
+namespace COMP123_week10
+{
+    /// <summary>
+    /// This is the SuperHero class
+    /// </summary>
+    public class SuperVillain : SuperHuman, IHasKarma
+    {
+        // PRIVATE INSTANCE VARIABLES
+        private int _karma;
+
+        // PUBLIC PROPERTIES
+        public int Darma
+        {
+            get
+            {
+                return this._darma;
+            }
+
+            set
+            {
+                this._darma = value;
+            }
+        }
+
+        // CONSTRUCTORS
+
+        /// <summary>
+        /// This is the main constructor for the SuperVillain class
+        /// It takes two arguments - name (string) - darma (int)
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="darma"></param>
+        public SuperVillain(string name, int darma)
+            : base(name)
+        {
+            this.Darma = darma;
+        }
+
+        // PRIVATE METHODS
+
+        // PUBLIC METHODS
+    }
+}
