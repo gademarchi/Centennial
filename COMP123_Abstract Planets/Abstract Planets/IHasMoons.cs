a2573942622cccc92a25036e833184368bd478e4
Gabriel <Gabriel@GABRIEL-PC>
2017-07-17 12:03:11 -0400

    create IHasMoons

---
+++ COMP123_Abstract Planets/Abstract Planets/IHasMoons.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Gabriel De Marchi
+ * Date: July 17, 2017  12:02am
+ * Version: 0.1 - declared the IHasMoons interface
+ */
+
+namespace Abstract_Planets
+{
+
+    public interface IHasMoons
+    {
+        bool HasMoons();
+    }
+}
