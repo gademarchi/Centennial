35341695ada1826a6d685fa292ae5ae2fef9b55c
Gabriel <Gabriel@GABRIEL-PC>
2017-07-17 11:57:03 -0400

    create Habitable and IhasRings

---
+++ COMP123_Abstract Planets/Abstract Planets/IHasRings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Gabriel De Marchi
+ * Date: July 17, 2017
+ * Version: 0.1 - declared the IHasRings property
+ */
+
+
+namespace Abstract_Planets
+{
+    public interface IHasRings
+    {
+        bool HasRings();
+
+    }
+}
--- COMP123_Abstract Planets/Abstract Planets/TerrestrialPlanet.cs
+++ COMP123_Abstract Planets/Abstract Planets/TerrestrialPlanet.cs
@@ -11,11 +11,41 @@ namespace Abstract_Planets
         private bool _oxygen;
 
         //constructors
-        public TerrestrialPlanet(string name, double diameter, double mass, bool oxygen) : base(name,diameter,mass)
+        public TerrestrialPlanet(string name, double diameter, double mass, bool oxygen) 
+            : base(name,diameter,mass)
         {
             this._oxygen = oxygen;
         }
 
+        //public method
+        public bool Habitable()
+        {
+           if(_oxygen == true)
+            {
+                _oxygen = true;
+            }
+            else
+            {
+                _oxygen = false;
+            }
+            return _oxygen;
+
+        }
+
+        public bool HasMoons()
+        {
+            bool moon;
+
+            if (MoonCount > 0)
+            {
+                moon = true;
+            }
+            else
+            {
+                moon = false;
+            }
+            return moon;
+        }
 
 
 
