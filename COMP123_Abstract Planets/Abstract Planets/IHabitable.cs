2b347e5cb5bab80743c3c5627d0c46eced19d873
Gabriel <Gabriel@GABRIEL-PC>
2017-07-17 12:01:16 -0400

    Create Ihabitable

---
+++ COMP123_Abstract Planets/Abstract Planets/IHabitable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Gabriel De Marchi
+ * Date: July 17, 2017 11:59am
+ * Version: 0.1 - declared the IHabitable property
+ */
+
+namespace Abstract_Planets
+{
+    public interface IHabitable
+    {
+        bool Habitable();
+    }
+}
