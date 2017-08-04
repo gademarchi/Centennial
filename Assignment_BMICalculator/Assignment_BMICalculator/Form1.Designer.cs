defb10303a99dbad5b2e300f68cb1331e5231baa
Gabriel <Gabriel@GABRIEL-PC>
2017-08-03 21:58:21 -0400

    form created

--- Assignment_BMICalculator/Assignment_BMICalculator/Form1.Designer.cs
+++ Assignment_BMICalculator/Assignment_BMICalculator/Form1.Designer.cs
@@ -63,9 +63,9 @@
             this.tableLayoutPanel1.RowCount = 5;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 61.11111F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 38.88889F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 42F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 93F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 171F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 47F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 119F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 152F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(315, 455);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
@@ -74,7 +74,7 @@
             this.rdImperial.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.rdImperial.AutoSize = true;
             this.rdImperial.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rdImperial.Location = new System.Drawing.Point(4, 27);
+            this.rdImperial.Location = new System.Drawing.Point(4, 23);
             this.rdImperial.Name = "rdImperial";
             this.rdImperial.Size = new System.Drawing.Size(150, 35);
             this.rdImperial.TabIndex = 0;
@@ -87,7 +87,7 @@
             this.rdMetric.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.rdMetric.AutoSize = true;
             this.rdMetric.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rdMetric.Location = new System.Drawing.Point(161, 27);
+            this.rdMetric.Location = new System.Drawing.Point(161, 23);
             this.rdMetric.Name = "rdMetric";
             this.rdMetric.Size = new System.Drawing.Size(150, 35);
             this.rdMetric.TabIndex = 1;
@@ -99,7 +99,7 @@
             // 
             this.txtWeight.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.txtWeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtWeight.Location = new System.Drawing.Point(161, 148);
+            this.txtWeight.Location = new System.Drawing.Point(161, 137);
             this.txtWeight.Name = "txtWeight";
             this.txtWeight.Size = new System.Drawing.Size(150, 38);
             this.txtWeight.TabIndex = 3;
@@ -108,7 +108,7 @@
             // 
             this.txtHeight.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.txtHeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtHeight.Location = new System.Drawing.Point(4, 148);
+            this.txtHeight.Location = new System.Drawing.Point(4, 137);
             this.txtHeight.Name = "txtHeight";
             this.txtHeight.Size = new System.Drawing.Size(150, 38);
             this.txtHeight.TabIndex = 2;
@@ -118,7 +118,7 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(11, 101);
+            this.label1.Location = new System.Drawing.Point(11, 91);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(136, 31);
             this.label1.TabIndex = 4;
@@ -129,7 +129,7 @@
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(165, 101);
+            this.label2.Location = new System.Drawing.Point(165, 91);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(141, 31);
             this.label2.TabIndex = 5;
@@ -141,9 +141,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCalculate.Location = new System.Drawing.Point(4, 191);
+            this.btnCalculate.Location = new System.Drawing.Point(4, 184);
             this.btnCalculate.Name = "btnCalculate";
-            this.btnCalculate.Size = new System.Drawing.Size(150, 87);
+            this.btnCalculate.Size = new System.Drawing.Size(150, 113);
             this.btnCalculate.TabIndex = 6;
             this.btnCalculate.Text = "C&alculate BMI";
             this.btnCalculate.UseVisualStyleBackColor = true;
@@ -154,9 +154,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(161, 191);
+            this.button2.Location = new System.Drawing.Point(161, 184);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(150, 87);
+            this.button2.Size = new System.Drawing.Size(150, 113);
             this.button2.TabIndex = 7;
             this.button2.Text = "&Close";
             this.button2.UseVisualStyleBackColor = true;
@@ -166,7 +166,7 @@
             this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 352);
+            this.label3.Location = new System.Drawing.Point(12, 362);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(134, 31);
             this.label3.TabIndex = 8;
@@ -176,7 +176,7 @@
             // 
             this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(161, 349);
+            this.textBox1.Location = new System.Drawing.Point(161, 358);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(150, 38);
             this.textBox1.TabIndex = 9;
