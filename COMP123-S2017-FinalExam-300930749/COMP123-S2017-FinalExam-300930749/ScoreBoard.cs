3ecbb69756f55da656061a9d61faba99a425ca87
Gabriel <Gabriel@GABRIEL-PC>
2017-08-17 11:51:29 -0400

    score board class created

---
+++ COMP123-S2017-FinalExam-300930749/COMP123-S2017-FinalExam-300930749/ScoreBoard.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace COMP123_S2017_FinalExam_300930749
+{
+    public class ScoreBoard
+    {
+
+        public TextBox _finalScoreTextBox;
+        public int _score;
+        public int _time;
+        public TextBox _timeTextBox;
+
+
+         public TextBox FinalScoreBox { get; set; }
+        public int Score { get; set; }
+        public TextBox ScoreTextBox { get; set; }
+        public int Time { get; set; }
+        public TextBox TimeTextBox { get; set; }
+
+
+    }
+}
